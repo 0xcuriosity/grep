b40f282d510982b6a749b406526b75ed6e9f0ee1
Tunnel_Rat <reachashish2004@gmail.com>
2025-08-22 11:19:56 +0530

    iterators included

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ use std::error::Error;
 use std::fs;
 use std::process;
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -23,12 +22,16 @@ struct Config {
     ignore_case: bool,
 }
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(query) => query,
+            None => return Err("Didnt give a query string"),
+        };
+        let file_path = match args.next() {
+            Some(path) => path,
+            None => return Err("Didnt give a file path"),
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
         Ok(Config {
             query,
